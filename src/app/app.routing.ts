fb9342a9708f8cd40a05408c700cab336d8cfb14
Adam Graham <adam@plasticmobile.com>
2018-09-22 11:02:26 -0400

    mock up data

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule, } from '@angular/common';
-import { BrowserModule  } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
@@ -11,24 +11,26 @@ import { IconsComponent } from './icons/icons.component';
 import { MapsComponent } from './maps/maps.component';
 import { NotificationsComponent } from './notifications/notifications.component';
 
-const routes: Routes =[
-    { path: 'dashboard',      component: HomeComponent },
-    { path: 'user',           component: UserComponent },
-    { path: 'table',          component: TablesComponent },
-    { path: 'typography',     component: TypographyComponent },
-    { path: 'icons',          component: IconsComponent },
-    { path: 'maps',           component: MapsComponent },
-    { path: 'notifications',  component: NotificationsComponent },
-    { path: '',          redirectTo: 'dashboard', pathMatch: 'full' }
+const routes: Routes = [
+	{ path: 'dashboard', component: HomeComponent },
+	{ path: 'user', component: UserComponent },
+	{ path: 'table', component: TablesComponent },
+	{ path: 'typography', component: TypographyComponent },
+	{ path: 'icons', component: IconsComponent },
+	{ path: 'maps', component: MapsComponent },
+	{ path: 'notifications', component: NotificationsComponent },
+	{ path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+	{ path: 'dashboard/:library', component: HomeComponent },
+
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    BrowserModule,
-    RouterModule.forRoot(routes)
-  ],
-  exports: [
-  ],
+	imports: [
+		CommonModule,
+		BrowserModule,
+		RouterModule.forRoot(routes)
+	],
+	exports: [
+	],
 })
 export class AppRoutingModule { }
--- src/app/notifications/notifications.component.ts
+++ src/app/notifications/notifications.component.ts
@@ -1,32 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
-declare var $:any;
+declare var $: any;
 
 @Component({
-  selector: 'app-notifications',
-  templateUrl: './notifications.component.html',
-  styleUrls: ['./notifications.component.css']
+	selector: 'app-notifications',
+	templateUrl: './notifications.component.html',
+	styleUrls: ['./notifications.component.css']
 })
 export class NotificationsComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
-  showNotification(from, align){
-      const type = ['','info','success','warning','danger'];
+	ngOnInit() {
+	}
+	showNotification(from, align) {
+		const type = ['', 'info', 'success', 'warning', 'danger'];
 
-      var color = Math.floor((Math.random() * 4) + 1);
-      $.notify({
-          icon: "pe-7s-gift",
-          message: "Welcome to <b>Light Bootstrap Dashboard</b> - a beautiful freebie for every web developer."
-      },{
-          type: type[color],
-          timer: 1000,
-          placement: {
-              from: from,
-              align: align
-          }
-      });
-  }
+		var color = Math.floor((Math.random() * 4) + 1);
+		$.notify({
+			icon: "pe-7s-gift",
+			message: "Welcome to <b>Light Bootstrap Dashboard</b> - a beautiful freebie for every web developer."
+		}, {
+				type: type[color],
+				timer: 1000,
+				placement: {
+					from: from,
+					align: align
+				}
+			});
+	}
 }
